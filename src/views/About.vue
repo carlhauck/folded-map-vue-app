02d7b72bbee2129856f7a69be9555472df764de2
carlhauck <carlhauck@gmail.com>
2020-07-21 14:37:15 -0500

    installs axios and makes other preliminary tweaks

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,17 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+    };
+  },
+  created: function() {},
+  methods: {}
+};
+</script>
