edf58842d216ae3d580dbddddfc35ebe03b3d973
carlhauck <carlhauck@gmail.com>
2020-07-28 11:35:13 -0500

    tweaks formatting in ConversationsIndex and revises About info

--- src/views/ConversationsIndex.vue
+++ src/views/ConversationsIndex.vue
@@ -5,8 +5,8 @@
         <h3><router-link class="nav-link" :to="`/users/${conversation.partner.id}`">{{ conversation.partner.display_name }}</router-link> <span v-if="conversation.map_twin">– Map Twin</span></h3>
         <p v-if="conversation.last_message">{{ conversation.last_message.text }}</p>
         <p v-if="conversation.last_message"><small>{{ sentRelativeTime(conversation.last_message.created_at) }}</small></p>
-        <router-link v-if="conversation.last_message" class="nav-link" :to="`/conversations/${conversation.id}`">See conversation</router-link>
-        <router-link v-if="!conversation.last_message" class="nav-link" :to="`/conversations/${conversation.id}`">Start the conversation</router-link>
+        <p><router-link v-if="conversation.last_message" class="nav-link" :to="`/conversations/${conversation.id}`">See conversation</router-link>
+        <router-link v-if="!conversation.last_message" class="nav-link" :to="`/conversations/${conversation.id}`">Start the conversation</router-link></p>
     </div>
   </div>
 </template>
