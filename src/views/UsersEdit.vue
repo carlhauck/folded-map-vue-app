b7fbd3e80fbf7527cc716d929c0f4dd2ae76b8e3
carlhauck <carlhauck@gmail.com>
2020-08-03 13:40:44 -0500

    updates UserEdit view style

--- src/views/UsersEdit.vue
+++ src/views/UsersEdit.vue
@@ -1,101 +1,134 @@
 <template>
   <div class="users-edit">
-    <form v-on:submit.prevent="updateUser()">
-      <h1>Edit Profile</h1>
-      <ul>
-        <li class="text-danger" v-for="error in errors">{{ error }}</li>
-      </ul>
-      <div class="form-group">
-        <label>First name:</label>
-        <ValidationProvider name="First name" rules="alpha" v-slot="{ errors }">
-          <input type="text" class="form-control" v-model="user.first_name">
-          <span>{{ errors[0] }}</span>
-        </ValidationProvider>
-      </div>
-      <div class="form-group">
-        <label>Last name:</label>
-        <ValidationProvider name="Last name" rules="alpha" v-slot="{ errors }">
-          <input type="text" class="form-control" v-model="user.last_name">
-          <span>{{ errors[0] }}</span>
-        </ValidationProvider>
-      </div>
-      <div class="form-group">
-        <label>House number:</label>
-        <ValidationProvider name="House number" rules="numeric" v-slot="{ errors }">
-          <input type="text" size="5" maxlength="5" class="form-control" v-model="user.street_num">
-          <span>{{ errors[0] }}</span>
-        </ValidationProvider>
-      </div>
-      <div class="form-group">
-        <label>Street direction:</label>
-        <select name="direction" id="direction" v-model="user.street_direction">
-          <option value="N">N</option>
-          <option value="S">S</option>
-          <option value="E">E</option>
-          <option value="W">W</option>
-        </select>
-       </div>
-      <div class="form-group">
-        <label>Street:</label>
-        <input type="text" maxlength="45" class="form-control" v-model="user.street">
-      </div>
-      <div class="form-group">
-        <label>ZIP code:</label>
-        <ValidationProvider name="ZIP code" rules="numeric" v-slot="{ errors }">
-          <input type="text" size="5" maxlength="5" class="form-control" v-model="user.zip_code">
-          <span>{{ errors[0] }}</span>
-        </ValidationProvider>
-      </div>
-      <div class="form-group">
-        <label>Email:</label>
-        <ValidationProvider name="email" rules="required|email" v-slot="{ errors }">
-          <input type="text" class="form-control" v-model="user.email">
-          <span>{{ errors[0] }}</span>
-        </ValidationProvider>
-      </div>
-      <div class="form-group">
-        <label>Old password:</label>
-        <input type="password" class="form-control" v-model="oldPassword">
-      </div>
-      <ValidationObserver>
-        <div class="form-group">
-          <label>New password:</label>
-          <ValidationProvider v-slot="{ errors }" vid="confirmation">
-            <input type="password" class="form-control" v-model="password">
-            <span>{{ errors[0] }}</span>
-          </ValidationProvider>
+    <div class="section section-gray">
+      <div class="container container-tim">
+        <div class="row">
+          <div class="col-md-8 ml-auto mr-auto">
+            <form v-on:submit.prevent="updateUser()">
+              <h2>Edit Profile</h2>
+              <ul>
+                <li class="text-danger" v-for="error in errors">{{ error }}</li>
+              </ul>
+              <div class="form-group">
+                <div class="row">
+                  <div class="col">
+                    <label>First name:</label>
+                    <ValidationProvider name="First name" rules="required|alpha" v-slot="{ errors }">
+                      <input type="text" class="form-control" v-model="user.first_name">
+                      <small class="form-text text-muted">{{ errors[0] }}</small>
+                    </ValidationProvider>
+                  </div>
+                  <div class="col">
+                    <label>Last name:</label>
+                    <ValidationProvider name="Last name" rules="required|alpha" v-slot="{ errors }">
+                      <input type="text" class="form-control" v-model="user.last_name">
+                      <small class="form-text text-muted">{{ errors[0] }}</small>
+                    </ValidationProvider>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="row">
+                  <div class="col">
+                    <label>House number:</label>
+                    <ValidationProvider name="House number" rules="required|numeric" v-slot="{ errors }">
+                      <input type="text" maxlength="5" class="form-control" v-model="user.street_num">
+                      <small class="form-text text-muted">{{ errors[0] }}</small>
+                    </ValidationProvider>
+                  </div>
+                  <div class="col">
+                    <label for="direction">Street direction:</label>
+                    <select id="direction" class="form-control" v-model="user.street_direction">
+                      <option value="N">N</option>
+                      <option value="S">S</option>
+                      <option value="E">E</option>
+                      <option value="W">W</option>
+                    </select>
+                  </div>
+                  <div class="col-md-4">
+                    <label>Street:</label>
+                    <ValidationProvider name="Street" rules="required" v-slot="{ errors }">
+                      <input type="text" maxlength="45" class="form-control" v-model="user.street">
+                      <span>{{ errors[0] }}</span>
+                    </ValidationProvider>
+                  </div>
+                  <div class="col-md-2">
+                    <label>ZIP code:</label>
+                    <ValidationProvider name="ZIP code" rules="required|numeric" v-slot="{ errors }">
+                      <input type="text" size="5" maxlength="5" class="form-control" v-model="user.zip_code">
+                      <span>{{ errors[0] }}</span>
+                    </ValidationProvider>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <label>Email:</label>
+                <ValidationProvider name="email" rules="required|email" v-slot="{ errors }">
+                  <input type="text" class="form-control" v-model="user.email">
+                  <span>{{ errors[0] }}</span>
+                </ValidationProvider>
+              </div>
+              <div class="form-group">
+                <label>Old password:</label>
+                <input type="password" class="form-control" v-model="oldPassword">
+              </div>
+              <ValidationObserver>
+                <div class="form-group">
+                  <label>New password:</label>
+                  <ValidationProvider v-slot="{ errors }" vid="confirmation">
+                    <input type="password" class="form-control" v-model="password">
+                    <span>{{ errors[0] }}</span>
+                  </ValidationProvider>
+                </div>
+                <div class="form-group">
+                  <label>Password confirmation:</label>
+                  <ValidationProvider rules="confirmed:confirmation" v-slot="{ errors }">
+                    <input type="password" class="form-control" v-model="passwordConfirmation">
+                    <span>{{ errors[0] }}</span>
+                  </ValidationProvider>
+                </div>
+              </ValidationObserver>
+              <div class="form-group">
+                <div class="row">
+                  <div class="col-md-3">
+                    <label>Birthday:</label>
+                    <input type="date" class="form-control" v-model="user.birthday">
+                  </div>
+                  <div class="col-md-9">
+                    <label>Image URL:</label>
+                    <input type="text" class="form-control" v-model="user.image_url">
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <label>How you came to live in your neighborhood:</label>
+                <textarea class="form-control" id="howIGotHere" rows="3" v-model="user.how_i_got_here"></textarea>
+              </div>
+              <div class="form-group">
+                <label>What you like about where you live:</label>
+                <textarea class="form-control" id="howIGotHere" rows="3" v-model="user.what_i_like"></textarea>
+              </div>
+              <div class="form-group">
+                <label>What you'd change about where you live:</label>
+                <textarea class="form-control" id="howIGotHere" rows="3" v-model="user.what_i_would_change"></textarea>
+              </div>
+              <div class="form-group">
+                <div class="row">
+                  <div class="col"></div>
+                  <div class="col text-center">
+                    <input type="submit" class="btn btn-info btn-round" value="Update Profile">
+                  </div>
+                  <div class="col text-center">
+                    <button class="btn btn-warning btn-round" v-on:click="destroyUser()">Delete Account</button>
+                  </div>
+                  <div class="col"></div>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-        <div class="form-group">
-          <label>Password confirmation:</label>
-          <ValidationProvider rules="confirmed:confirmation" v-slot="{ errors }">
-            <input type="password" class="form-control" v-model="passwordConfirmation">
-            <span>{{ errors[0] }}</span>
-          </ValidationProvider>
-        </div>
-      </ValidationObserver>
-      <div class="form-group">
-        <label>Birthday:</label>
-        <input type="date" class="form-control" v-model="user.birthday">
       </div>
-      <div class="form-group">
-        <label>Image URL:</label>
-        <input type="text" class="form-control" v-model="user.image_url">
-      </div>
-      <div class="form-group">
-        <label>How you came to live in your neighborhood:</label>
-        <input type="text" class="form-control" v-model="user.how_i_got_here">
-      </div>
-      <div class="form-group">
-        <label>What you like about where you live:</label>
-        <input type="text" class="form-control" v-model="user.what_i_like">
-      </div>
-      <div class="form-group">
-        <label>What you'd change about where you live:</label>
-        <input type="text" class="form-control" v-model="user.what_i_would_change">
-      </div>
-      <input type="submit" class="btn btn-primary" value="Update Profile">
-    </form>
-    <button v-on:click="destroyUser()">Delete Account</button>
+    </div>
   </div>
 </template>
 
